3fdeaaa2b38c20d22944d34a087bda2c782de532
christiansanchez-SE <christiansanchez714@gmail.com>
2024-07-14 16:33:55 -0400

    added ToDoList.jsx and data folder

---
+++ src/data/ToDoList.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+const todoList = [
+    {
+        id: 1,
+        title: "Todo:",
+        task: "Practice coding",
+        complete: false
+    },
+    {
+        id: 2,
+        title: "Todo:",
+        task: "Take out the trash",
+        complete: false
+    },
+    {
+        id: 3,
+        title: "Todo:",
+        task: "Walk dog",
+        complete: false
+    }
+];
+
+export default todoList
